b011efe358ad41ef85792d21c5b71a32adafbd26
trips4 <dennis.triplo@perforce.comm>
2025-10-01 13:13:13 -0500

    new class

---
+++ manifests/package.pp
@@ -0,0 +1,13 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include bginfo::package
+class bginfo::package {
+  file { 'BGInfo64.exe':
+    ensure => 'file',
+    path   => 'C:/Windows/BGInfo64.exe',
+    source => 'puppet:///modules/bginfo/BGInfo64.exe',
+  }
+}
