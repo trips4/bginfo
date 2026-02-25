19c96b06248f5102ed71f13f55da0717367a6e85
trips4 <dennis.triplo@perforce.comm>
2026-02-25 11:26:22 -0600

    update

--- manifests/file.pp
+++ manifests/file.pp
@@ -1,16 +1,15 @@
 class bginfo::file {
-  # $bginfo_dir = 'c:/BGInfo'
-  # $bginfo_config = "${bginfo_dir}/PuppetFacts.bgi"
-  # $bginfo_dat = "${bginfo_dir}/PuppetBGInfo.dat"
 
   $bgi_cfg_file = 'PuppetFacts.bgi'
   $bgi_dat_file = 'PuppetBGInfo.dat'
   $bgi_dir      = 'BGInfo'
   $bgi_path     = 'c:/'
 
+  # Ensure the BGInfo directory exists.
   file { "${bgi_path}${bgi_dir}":
     ensure => directory,
   }
+  # Ensure the .dat and .bgi files exist, with the .bgi file sourced from the module.
   file { "${bgi_path}${bgi_dir}/${bgi_dat_file}":
     ensure  => file,
     require => File["${bgi_path}${bgi_dir}"],
@@ -20,7 +19,7 @@ class bginfo::file {
     source  => 'puppet:///modules/bginfo/PuppetFacts.bgi',
     require => File["${bgi_path}${bgi_dir}"],
   }
-
+  # Populate the .dat file with the specified facts and their values.
   $bginfo::displayed_facts.each |String $fact| {
     $fact_parts = $fact.split('[.]')
     $fact_value = dig($facts, *$fact_parts)
