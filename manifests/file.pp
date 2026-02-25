93b3bafc30b822049bf4c8410893c0fa2d0fd99a
trips4 <dennis.triplo@perforce.comm>
2026-02-25 08:54:36 -0600

    update

--- manifests/file.pp
+++ manifests/file.pp
@@ -19,7 +19,8 @@ class bginfo::file {
   $bginfo::displayed_facts.each |String $fact| {
     notify { "The Fact to Process = ${fact}":
     }
-    $fact_parts = split($fact, '[.]')
+    #$fact_parts = split($fact, '[.]') Works
+    $fact_parts = $fact.split('.')
     notify { "Var fact_parts = ${fact_parts}": }
     # $fact_path  = $fact_parts.map |$part| { "['${part}']" }.join('.')
     # notify { "Fact path for ${fact}: ${fact_path}":
