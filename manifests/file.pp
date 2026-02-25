cf646ca024be2a623ec1af0aae6e239115f0e99c
trips4 <dennis.triplo@perforce.comm>
2026-02-25 14:02:10 -0600

    trying new bits

--- manifests/file.pp
+++ manifests/file.pp
@@ -25,7 +25,6 @@ class bginfo::file {
     if $fact_parts[0] == 'trusted' {
       $trusted_parts = $fact_parts[1, $fact_parts.length - 1]
       $fact_value    = dig($trusted, *$trusted_parts)
-      notify { "This was trusted parts ${trusted_parts}": }
     } else {
       $fact_value = dig($facts, *$fact_parts)
     }
