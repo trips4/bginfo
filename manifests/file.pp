ea95c57d789c76abd16274b79b4ee209cc0807d1
trips4 <dennis.triplo@perforce.comm>
2026-02-25 15:28:20 -0600

    added new test

--- manifests/file.pp
+++ manifests/file.pp
@@ -9,32 +9,13 @@ class bginfo::file {
   file { "${bgi_path}${bgi_dir}":
     ensure => directory,
   }
-  # # Ensure the .dat and .bgi files exist, with the .bgi file sourced from the module.
-  # file { "${bgi_path}${bgi_dir}/${bgi_dat_file}":
-  #   ensure  => file,
-  #   require => File["${bgi_path}${bgi_dir}"],
-  # }
+
   file { "${bgi_path}${bgi_dir}/${bgi_cfg_file}":
     ensure  => file,
     source  => 'puppet:///modules/bginfo/PuppetFacts.bgi',
     require => File["${bgi_path}${bgi_dir}"],
   }
-  # # Populate the .dat file with the specified facts and their values.
-  # $bginfo::displayed_facts.each |String $fact| {
-  #   $fact_parts = $fact.split('[.]')
-  #   if $fact_parts[0] == 'trusted' {
-  #     $trusted_parts = $fact_parts[1, $fact_parts.length - 1]
-  #     $fact_value    = dig($trusted, *$trusted_parts)
-  #   } else {
-  #     $fact_value = dig($facts, *$fact_parts)
-  #   }
 
-  #   file_line { "BGInfo Task ${fact}":
-  #     ensure => present,
-  #     path   => "${bgi_path}${bgi_dir}/${bgi_dat_file}",
-  #     line   => "${fact}: ${fact_value}",
-  #   }
-  # }
   file { "${bgi_path}${bgi_dir}/${bgi_dat_file}":
     ensure  => file,
     content => epp('bginfo/bginfo.epp'),
