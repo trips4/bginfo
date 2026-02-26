b9179412a5e0400d8054adf4ddbae341f91ec117
trips4 <dennis.triplo@perforce.comm>
2026-02-26 15:17:22 -0600

    fixed testing

--- spec/classes/file_spec.rb
+++ spec/classes/file_spec.rb
@@ -9,7 +9,21 @@ describe 'bginfo::file' do
       let(:pre_condition) do
         "class { 'bginfo': displayed_facts => ['trusted.extensions.pp_role', 'os.family'] }"
       end
+
       it { is_expected.to compile.with_all_deps }
+      it do
+        is_expected.to contain_file('c:/BGInfo')
+          .with_ensure('directory')
+      end
+      it do
+        is_expected.to contain_file('c:/BGInfo/PuppetFacts.bgi')
+          .with_ensure('file')
+          .with_source('puppet:///modules/bginfo/PuppetFacts.bgi')
+      end
+      it do
+        is_expected.to contain_file('c:/BGInfo/PuppetBGInfo.dat')
+          .with_ensure('file')
+      end
     end
   end
 end
