56e896adcc57e7dd1fb75f5646936aa2f002f61c
trips4 <dennis.triplo@perforce.comm>
2026-02-26 10:44:00 -0600

    update

--- manifests/package.pp
+++ manifests/package.pp
@@ -13,6 +13,6 @@ class bginfo::package {
     ensure => present,
     path   => 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\BGInfo',
     type   => string,
-    data   => 'hello',
+    data   => 'c:\bginfo\BGInfo64.exe C:\BGInfo\PuppetFacts.bgi /SILENT /NOLICPROMPT',
   }
 }
